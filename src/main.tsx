f1202f939ef76d5f5d76c61bd3ac561428242835
juice-hh <juicehh0626@gmail.com>
2026-04-30 00:32:08 +0800

    feat: add chat UI components and useChat hook

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { ChatProvider } from './context/ChatContext'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChatProvider>
-      <App />
-    </ChatProvider>
+    <App />
   </React.StrictMode>,
 )
